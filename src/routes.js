4c0b83677955858cd39a058a2cf846ef823a0421
PauloRC0 <prcs3@discente.ifpe.edu.br>
2025-02-15 01:58:43 -0300

    feat: adicionando metodo post e get na task

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,18 @@
 const {Router} = require('express')
-
+const Task = require('./models/tasks')
 const routes = Router() 
 
-routes.get('/', (req, res) => {
-    res.send('Hello World')
+routes.get('/tasks', async (req, res) => {
+    const tasks = await Task.findAll()
+
+    return res.json(tasks)
 })
 
+routes.post('/tasks', async (req, res) => {
+    const {title, status, priority, description} = req.body
+    
+    const task = await Task.create({title, status, priority, description})
+
+    return res.json(task)
+})
 module.exports = routes
