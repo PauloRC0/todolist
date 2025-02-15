99631a43d3fe31d07be7ff2f1de6319f755c4891
PauloRC0 <prcs3@discente.ifpe.edu.br>
2025-02-15 00:53:15 -0300

    feat: conexão com o banco de dados

---
+++ src/index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const routes = require('./routes')
+const app = express()
+const port = 3000
+
+require('./database')
+app.use(express.json())
+app.use(require('./routes'))
+
+app.listen(3000 , () => {
+    console.log(`Servidor rodando na porta ${port}`)
+})
