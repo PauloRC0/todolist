b8111c7bc87f359d37ba8d82f7fe36d3b17dfae2
PauloRC0 <prcs3@discente.ifpe.edu.br>
2025-03-14 19:54:23 -0300

    feat: adicionando rota do get

--- src/routes.js
+++ src/routes.js
@@ -4,12 +4,7 @@ const {Router} = require('express')
 const UserController = require('../src/controller/UserController')
 const routes = Router() 
 
-routes.get('/tasks', async (req, res) => {
-    const tasks = await Task.findAll()
-
-    return res.json(tasks)
-})
-
+routes.get('/tasks-list',UserController.listTasks)
 routes.post('/tasks-create',UserController.createTasks)
 routes.put('/tasks-update/:id',UserController.updateTasks) 
 module.exports = routes
