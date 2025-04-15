20cee05533f2cbd346ee80a83dc534b7f8072b00
PauloRC0 <prcs3@discente.ifpe.edu.br>
2025-04-15 00:38:40 -0300

    feat : rota de tag

--- src/routes.js
+++ src/routes.js
@@ -2,10 +2,14 @@ const {Router} = require('express')
 
 
 const TaskController = require('../src/controller/TaskController')
+const TagsController = require('../src/controller/TagsController')
 const routes = Router() 
 
 routes.get('/tasks-list',TaskController.listTasks)
 routes.post('/tasks-create',TaskController.createTasks)
 routes.put('/tasks-update/:id',TaskController.updateTasks) 
 routes.delete('/tasks-delete/:id',TaskController.deleteTasks)
+
+routes.post('/tags-create',TagsController.createTags)
+
 module.exports = routes
